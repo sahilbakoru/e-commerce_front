5edbe97303fada141a89366fdb4857fb3f417376
sahilbakoru <sahilbakoru1999@gmail.com>
2021-10-08 22:54:40 +0530

    signup_form_first_build.

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,9 +2,32 @@ import React from 'react';
 import Layout from './Layout' 
 
 
-const Home = () => (
-   <Layout title="Home page " discription ="node react e_commerce app" >
-       ....
-   </Layout>
-)
+const Home = () => {
+    const signUpForm =()=>(
+        <form>
+            <div className="form-group"> 
+                <label className="text-muted"> Name </label>
+                <input type="text" className="form-control"/>
+            </div>
+
+            <div className="form-group"> 
+                <label className="text-muted"> Email </label>
+                <input type="email" className="form-control"/>
+            </div>
+
+            <div className="form-group"> 
+                <label className="text-muted"> Password </label>
+                <input type="password" className="form-control"/>
+            </div>
+           
+        </form>
+    )
+
+
+    return (
+        <Layout title="Home page " discription ="node react e_commerce app" >
+            {signUpForm()}
+        </Layout>
+     )
+}
 export default Home
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { API } from '../config';
+import React,{useState} from 'react';
+//import { API } from '../config';
 import Layout from '../core/Layout'
 
 
-const Signup = () => (
-   <Layout title="Signup" discription ="Signup to node react e_commerce app" >
-      {API}
-   </Layout>
-)
+const Signup = () => {
+    const [values,setValues]=useState({ 
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success:false
+    })
+    const handleChange=name=>event=>{
+        setValues({...values,error:false,[name]:event.target.value})
+    }
+
+    const signUpForm =()=>(
+        <form>
+            <div className="form-group"> 
+                <label className="text-muted"> Name </label>
+                <input onChange={handleChange('name')} type="text" 
+                className="form-control"/>
+            </div>
+
+            <div className="form-group"> 
+                <label className="text-muted"> Email </label>
+                <input onChange={handleChange('email')} type="email" 
+                className="form-control"/>
+            </div>
+
+            <div className="form-group"> 
+                <label className="text-muted"> Password </label>
+                <input onChange={handleChange('password')} type="password" 
+                className="form-control"/>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    )
+
+
+    return (
+        <Layout title="Create account  " discription ="Signup to node react e_commerce app" 
+        className="container col-md-5 offset-md-2.5  " >
+            {signUpForm()}
+            {JSON.stringify(values)}
+        </Layout>
+     )
+}
 
 export default Signup
