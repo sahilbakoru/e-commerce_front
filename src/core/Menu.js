3a39b7b6dc5eb3a050cadd680441286fcbda60a4
sahilbakoru <sahilbakoru1999@gmail.com>
2021-10-12 12:38:48 +0530

    nothing

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -4,7 +4,7 @@ import {signout,isAuthenticated} from '../auth'
 
 const isActive =(history,path)=>{
     if(history.location.pathname === path){
-        return {color: '#02D64FF'};   // (blue) coror nav page on. 
+        return {color: '#02D64FF'};  // (blue) coror nav page on. 
     } else {
         return {color: '#000000'};  // (black) color nav page not on. 
     }
