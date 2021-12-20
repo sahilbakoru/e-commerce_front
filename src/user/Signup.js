2e2f6b63ff219559eab721d273b12f580efc3dd9
mohitgupta-byte <sahilbakoru1999@gmail.com>
2021-12-20 16:26:52 +0530

    hh

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
-import Layout from '../core/Layout'
+
 import { signup } from '../auth'
 import Menu from '../core/Menu';
 
